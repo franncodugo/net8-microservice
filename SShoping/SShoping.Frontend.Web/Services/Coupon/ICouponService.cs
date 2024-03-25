b45ab522050b4697fa2850df56ba25cc12fa4391
Franco <francodugo.vidal@gmail.com>
2024-03-25 20:11:12 -0300

    Interface for CouponService created on FE project

---
+++ SShoping/SShoping.Frontend.Web/Services/Coupon/ICouponService.cs
@@ -0,0 +1,12 @@
+﻿using SShoping.Frontend.Web.Models.Dtos;
+
+namespace SShoping.Frontend.Web.Services.Coupon;
+
+public interface ICouponService
+{
+    Task<ResponseDto?> GetAllCouponsAsync();
+    Task<ResponseDto?> GetCouponAsync(int couponId);
+    Task<ResponseDto?> PostCouponAsync(CouponDto couponDto);
+    Task<ResponseDto?> PutCouponAsync(CouponDto couponDto);
+    Task<ResponseDto?> DeleteCouponAsync(int couponId);
+}
