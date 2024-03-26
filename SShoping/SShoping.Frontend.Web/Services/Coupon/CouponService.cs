050281e7ba5ab9e1a8d5e9f42a307306441b9f39
Franco <francodugo.vidal@gmail.com>
2024-03-26 13:35:58 -0300

    Implementing ICouponService into the actual Service class. Adding Services Data to the appsettings file

---
+++ SShoping/SShoping.Frontend.Web/Services/Coupon/CouponService.cs
@@ -0,0 +1,39 @@
+﻿using SShoping.Frontend.Web.Models.Dtos;
+
+namespace SShoping.Frontend.Web.Services.Coupon;
+
+public class CouponService : ICouponService
+{
+    private readonly IBaseService _baseService;
+
+    public CouponService(IBaseService baseService)
+         => _baseService = baseService ?? throw new ArgumentNullException(nameof(baseService));
+
+    public async Task<ResponseDto?> GetAllCouponsAsync()
+        => await _baseService.SendAsync(new()
+        {
+            ApiType = Util.StaticDetails.ApiType.GET,
+            Url = "",
+            AccessToken = "",
+            Data = ""
+        });
+
+    public async Task<ResponseDto?> GetCouponAsync(int couponId)
+    {
+        throw new NotImplementedException();
+    }
+    public async Task<ResponseDto?> PostCouponAsync(CouponDto couponDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ResponseDto?> PutCouponAsync(CouponDto couponDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ResponseDto?> DeleteCouponAsync(int couponId)
+    {
+        throw new NotImplementedException();
+    }
+}
